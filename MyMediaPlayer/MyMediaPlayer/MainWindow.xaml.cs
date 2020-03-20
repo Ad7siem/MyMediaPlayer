1c04c267a1d023e1b16094a43c1716b88fafd8cc
Ad7siem <53908369+Ad7siem@users.noreply.github.com>
2020-03-20 13:24:57 +0100

    MyMediaPlayer

---
+++ MyMediaPlayer/MyMediaPlayer/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace MyMediaPlayer
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            inicjujTimer();
+        }
+
+        private void inicjujTimer()
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(250);
+            timer.Tick += (object sender, EventArgs e) =>
+            {
+                if (mediaElement.Source == null) lbOpis.Content = "<Nie wybrano pliku>";
+                {
+                    lbOpis.Content = mediaElement.Source.ToString();
+                    if (mediaElement.NaturalDuration.HasTimeSpan)
+                        lbOpis.Content += String.Format(" - {0} / {1}", mediaElement.Position.ToString(@"mm\:ss"),
+                            mediaElement.NaturalDuration.TimeSpan.ToString(@"mm\:ss"));
+                }
+            };
+            timer.Start();
+        }
+
+        #region Przyciski
+        private void btnOdtwarzaj_Click(object sender, RoutedEventArgs e)
+        {
+            mediaElement.Play();
+        }
+
+        private void btnWstrzymaj_Click(object sender, RoutedEventArgs e)
+        {
+            mediaElement.Pause();
+        }
+
+        private void btnZatrzymaj_Click(object sender, RoutedEventArgs e)
+        {
+            mediaElement.Stop();
+        }
+        #endregion
+    }
+}
